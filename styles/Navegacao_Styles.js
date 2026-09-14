ce4528db1c781a31213f434cae2dc2d8cee37334
diegororiz7 <diegororiz7@gmail.com>
2026-09-14 20:11:56 -0300

    Aula 14/09/2026 1

---
+++ styles/Navegacao_Styles.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  viewTN:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textoTN:{
+    fontSize: 20,
+    color: '#1c1c1c',
+  },
+  botaoContainer:{
+    width: '70%',
+    marginVertical: 5,
+  }
+});
+
+export default styles;
